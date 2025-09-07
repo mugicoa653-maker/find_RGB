68812033f858a134fcfb417f7b16ea576bb4a151
harunn-MainPC <haruharu41635@gmail.com>
2025-09-08 01:00:35 +0900

    正解との色の差異を計算

--- content.js
+++ content.js
@@ -21,15 +21,26 @@ function judge() {
   const rgbInput = document.querySelector("#rgb-input").value;
   console.log(rgbInput);
 
+  // 正解のRGB配列
   const rgbAnswerArr = rgbAnswer.match(/\d+/g).map(Number);
 
-  const hex = rgbInput;
-
+  // inputされたRGB配列
   const rgbArray = [
-    parseInt(hex.substr(0, 2), 16),
-    parseInt(hex.substr(2, 2), 16),
-    parseInt(hex.substr(4, 2), 16)
+    parseInt(rgbInput.substr(0, 2), 16),
+    parseInt(rgbInput.substr(2, 2), 16),
+    parseInt(rgbInput.substr(4, 2), 16)
   ];
 
-  
+  // r比較 (絶対値/(256/2))で正解の割合を出す (半分以上のズレは0)
+  let ansDiff = 100
+  for (let i=0; i<3; i++){
+    if(Math.abs(rgbAnswerArr[i] - rgbArray[i])<124){
+        let diff = Math.abs(rgbAnswerArr[i] - rgbArray[i]);
+        let diff_100 = diff/124;
+        let diff_33_1 = Math.round(diff_100/3*1000)/10;
+        ansDiff -= diff_33_1;
+    }else{
+        ansDiff -= 33;
+    }
+  }
 }
